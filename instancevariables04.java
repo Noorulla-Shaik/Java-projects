58e0af0cd50568e8fccee754fd92822a8057c7b4
Noorulla-shaik <24jk1a4257@gmail.com>
2025-07-24 20:20:56 +0530

    Instancevariables

---
+++ instancevariables04.java
@@ -0,0 +1,7 @@
+public class Instancevariables04 {
+    int value =10; //instance variable
+    public static void main(String[] args) {
+        Instancevariables04 obj = new Instancevariables04 (); //new consturcter for calling that variable
+        System.out.println("value ="+obj.value);
+    }
+}
