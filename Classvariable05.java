7787de058a1cc67e3ed7a3fa7f9e2402ab72a111
Noorulla-shaik <24jk1a4257@gmail.com>
2025-07-24 20:26:41 +0530

    CLASS variables

---
+++ Classvariable05.java
@@ -0,0 +1,7 @@
+public class Classvariable05 {
+    int values =10; //class variable
+    public static void main(String[] args) {
+        Classvariable05 obj = new Classvariable05();
+        System.out.println("values= " +obj.values );
+    }
+}
