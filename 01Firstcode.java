b3112868634f27f329b17cc220816030656e2085
Noorulla-shaik <24jk1a4257@gmail.com>
2025-07-24 19:44:34 +0530

    01Firstcode.java

---
+++ 01Firstcode.java
@@ -0,0 +1,15 @@
+class Firstcode { 
+    //firstcode is represent the file name
+    public static void main(String[] args) {
+        //PUBLIC → it is acess specifiers
+        //STATIC → which helps to create the memory block
+        //VOID → which doesnt return any value in main function
+        //MAIN → is a function
+        //String[] → This is an array of Strings
+        //ARGS → command line arguments
+        System.out.println("FIRST CODE IN JAVA LANGUAGE");
+        //system → class
+        //out → objects
+        //println → method , ln → next line
+    }
+}
