ffbb366a57a07c999442df8b4b5430fcaa4dd6d1
Noorulla-shaik <24jk1a4257@gmail.com>
2025-07-24 22:07:39 +0530

    STUDENT DETALIS USING SCANNER

---
+++ StudentDetails.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class StudentDetails {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("ENTER YOUR ROLL NUMBER :");
+        String number = s.nextLine();
+        System.out.println("ENTER YOUR NAME :");
+        String name = s.nextLine();
+        System.out.println("ENTER YOUR BRANCH :");
+        String branch = s.nextLine();
+        System.out.println("ENTER YOUR YEAR");
+        int year = s.nextInt();
+        System.out.println("YOUR DETALIS ARE ");
+System.out.println("NAME  ="+name);
+System.out.println("ROLL NUMBER ="+number);
+System.out.println("BRANCH ="+branch);
+System.out.println("YEAR ="+year);
+}
+}
