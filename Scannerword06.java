b06eceffd70ddb7fe3789c92fb224ca59a9edf48
Noorulla-shaik <24jk1a4257@gmail.com>
2025-07-24 20:37:17 +0530

    SCANNER FUNCTION USING

---
+++ Scannerword06.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Scannerword06 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("ENTER YOUR NAME :");
+        String name =s.nextLine();
+        System.out.println("YOUR NAME IS "+name);
+    }
+}
