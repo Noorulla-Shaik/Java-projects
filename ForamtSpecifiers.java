d7069183033ee177cdd59b80bb9c27fbe57c1acd
Noorulla-shaik <24jk1a4257@gmail.com>
2025-07-24 22:41:57 +0530

    FORMAT SPECIFIERS (%)

---
+++ ForamtSpecifiers.java
@@ -0,0 +1,11 @@
+// foramt specifiers same as c language
+class ForamtSpecifiers {
+    public static void main(String[] args) {
+        int number = 10;
+        String name = "YOUR NAME";
+        float value = 10.5f;
+        System.out.println("%d\n="+number);
+        System.out.println("%f\n="+value);
+        System.out.println("%s\n="+name);
+    }
+}
